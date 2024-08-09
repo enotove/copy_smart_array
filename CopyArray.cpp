ccc6d3463757bcd0f52c8d43e6b403b2b27d7d42
Enotove <enotove@gmail.com>
2024-08-09 19:19:06 +0500

    Init commit

---
+++ CopyArray.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+class Smart_array {
+public:
+    Smart_array(int a)
+    {
+        arr = new int[a];
+        size = a;
+    }
+
+    void add_element(int value)
+    {
+        if (arr == nullptr) throw std::exception("You're not make the array!");
+        else if (counter == size) throw std::exception("You fill in the array, that's enough");
+        arr[counter] = value;
+        counter++;
+
+    }
+    int get_element(int index)
+    {
+        if (index >= size)
+        {
+            throw std::exception("Going outside the array");
+        }
+        return arr[index];
+    }
+    Smart_array& operator = (Smart_array &array)
+    {
+        
+        this->arr = new int[array.size];
+        for (int i = 0; i < array.size; i++)
+        {
+            arr[i] = *(array.arr + i);
+        }
+       
+        return *this;
+    }
+    void Print_elements()
+    {
+        std::cout << "Elements in the array: ";
+        for (int i = 0; i < size; i++)
+        {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << '\n';
+    }
+    ~Smart_array()
+    {
+        if (arr != nullptr)
+        {
+            delete[] arr;
+            counter = 0;
+            arr = nullptr;
+        }
+    }
+
+
+private:
+    int* arr{ nullptr };
+    int counter = 0;
+    int size;
+
+};
+
+int main()
+{
+    Smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    Smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+    arr = new_array;
+    arr.Print_elements();
+}
